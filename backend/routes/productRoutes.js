31cf138a2ca2fc8fb65aaae1ab1adb19a1488f30
Vik-250 <123668475+Vik-250@users.noreply.github.com>
2024-09-29 20:41:34 +0530

    Add files via upload

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+const router = express.Router();
+import { getProducts, getProductById, getTopProducts} from "../controllers/productController.js";
+
+router.route('/').get(getProducts);
+router.get('/top', getTopProducts);
+router.route('/:id').get(getProductById);
+
+
+export default router;
---
+++ frontend/src/index.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+import {HelmetProvider} from 'react-helmet-async';
+import { Provider } from 'react-redux';
+import store from './store';
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/styles/bootstrap.custom.css';
+import './assets/styles/index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import ShippingScreen from './screens/ShippingScreen';
+import PrivateRoute from './components/PrivateRoute';
+import PaymentScreen from './screens/PaymentScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import OrderScreen from './screens/OrderScreen';
+
+
+const router =createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+      <Route index={true} path='/' element={<HomeScreen/>}/>
+      <Route path='/product/:id' element={<ProductScreen/>} />
+      <Route path='/cart' element={<CartScreen/>} />
+      <Route path='/search/:keyword' element={<HomeScreen/>} />
+      <Route path='/login' element={<LoginScreen/>} />
+      <Route path='/register' element={<RegisterScreen/>} />
+      
+      <Route path='' element={<PrivateRoute/>}>
+      <Route path='/shipping' element={<ShippingScreen/>} />
+      <Route path='/payment' element={<PaymentScreen/>} />
+      <Route path='/placeorder' element={<PlaceOrderScreen/>} />
+      <Route path='/profile' element={<ProfileScreen/>} />
+      <Route path='/order/:id' element={<OrderScreen />} />
+      </Route>
+    </Route>
+    
+
+  )
+)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+    <HelmetProvider>
+    <Provider store={store}>
+      <PayPalScriptProvider deferLoading={true}>
+      <RouterProvider router={router}/>
+      </PayPalScriptProvider>
+    
+    </Provider>
+    </HelmetProvider>  
+    </React.StrictMode>
+);
+
+
+reportWebVitals();
